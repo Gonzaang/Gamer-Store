e1efc99bfb7c83c11d2e8c638caa402485d719f5
gonza garnica <gonza.garnica@hotmail.com>
2020-10-19 19:54:01 -0300

    NavBar2

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 import Img from './components/Img';
 import Title from './components/Title'
 import ReactLink from './components/ReactLink'
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar/NavBar'
 import './App.css';
 
 function App (){
   return (<>
-      
-        <NavBar />
-        <Img />
-        <Title />
-        <ReactLink />    
+        <NavBar />   
     </>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import '../NavBar.css';
+import './NavBar.css';
 
 class NavBar extends React.Component{
 
   render(){
     return(<div>
       <nav >
-                <ul class="menu">
+            <ul class="menu">
                     <li><a href="index.html">Inicio</a></li>
                     <li><a href="nosotros.html">Nosotros</a></li>
                     <li><a href="productos.html">Productos</a></li>
                     <li><a href="contacto.html">Contacto</a></li>
-                </ul>
-            </nav>
+            </ul>
+      </nav>
     </div>
 
   
