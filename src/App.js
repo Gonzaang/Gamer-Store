fbbd58315224409a76cc350c0aec95c54e89acc6
mlacosta <marianoacosta.003@gmail.com>
2020-10-20 20:18:09 -0300

    Revert "components"
    This reverts commit 1e34ba7eb353a70cc6b8b2c7ff1d344ddbc53f61.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import Img from './components/Img';
 import Title from './components/Title'
@@ -9,6 +10,30 @@ function App (){
   return (<>
         <NavBar />   
     </>
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> parent of 1e34ba7... components
   );
 }
 
--- src/components/Title.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Title () {
-    return <p>
-    <strong>Gamer Store</strong>
-  </p>
-}
-
-export default Title;
